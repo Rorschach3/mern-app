f4839073f7169e76b73f5c4b60829f7bf605cc16
rorschach3 <daniel_fhernandez@yahoo.com>
2024-05-08 10:34:48 -0700

    OAUTH2 Access Token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
 import { Auth0Provider } from "@auth0/auth0-react";
-import { AppState } from '@auth0/auth0-react/dist/auth0-provider';
-import { User } from "@auth0/auth0-react";
-import { useCreateMyUser } from '@/api/MyUserApi';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
+
+  const navigate = useNavigate();
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -19,10 +17,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("Unable to initialize auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
+  const onRedirectCallback = () => {
+    navigate("/auth-callback")
   };
 
   return (

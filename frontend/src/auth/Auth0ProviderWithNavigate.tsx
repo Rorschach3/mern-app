97e6f4a513241a2b5f588ead61b3ed9151ceefdf
rorschach3 <daniel_fhernandez@yahoo.com>
2024-05-28 05:40:59 -0700

    update

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -6,15 +6,15 @@ type Props = {
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-
   const navigate = useNavigate();
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  // const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   if (!domain || !clientId || !redirectUri) {
-    throw new Error("Unable to initialize auth");
+    throw new Error("unable to initialize auth");
   }
 
   const onRedirectCallback = () => {
@@ -33,6 +33,6 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       {children}
     </Auth0Provider>
   );
-};
+};  
 
 export default Auth0ProviderWithNavigate;

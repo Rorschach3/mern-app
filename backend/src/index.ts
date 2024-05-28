88a3b3d085bd6a9f8ea6b50255e8da9fce5c414b
rorschach3 <daniel_fhernandez@yahoo.com>
2024-05-28 05:17:33 -0700

    Revert "Auth0 Access Token"
    This reverts commit 5a948f5297762fbd66fc2613145b03d888a1d81a.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,23 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
-import "dotenv/config";
 
-require('dotenv').config();
-// Connect to database
-
-mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(() => console.log("Connected to database!"));
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 app.use("/api/my/user", myUserRoute);
 
-console.log(process.env.AUTH0_ISSUER_BASE_URL);
 
-app.listen(7000, () => {
-    console.log("Server started on localhost:7000");
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ message: "Backend-Hello!" });
 });
 
-app.use((err: Error, req: Request, res: Response) => {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
-})
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,4 +1,3 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -40,6 +39,6 @@ export const useCreateMyUser =  () => {
         isLoading,
         isError,
         isSuccess,
+        };
     };
-};
 

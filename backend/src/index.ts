ab12ad51fc8dab65a6887fb4c1157bc9c2040ff1
rorschach3 <daniel_fhernandez@yahoo.com>
2024-05-30 04:50:52 -0700

    git conflicts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,23 +4,16 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 
-require('dotenv').config();
-// Connect to database
-
-mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(() => console.log("Connected to database!"));
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
-app.use(express.json());
 
 app.use("/api/my/user", myUserRoute);
 
-console.log(process.env.AUTH0_ISSUER_BASE_URL);
 
-app.listen(7000, () => {
-    console.log("Server started on localhost:7000");
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ message: "Backend-Hello!" });
 });
--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,5 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 type CreateUserRequest = {
@@ -19,7 +19,7 @@ export const useCreateMyUser =  () => {
                 Authorization: `Bearer ${accessToken}`,
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(user),
+                body: JSON.stringify(user),
         });
     
         if (!response.ok) {

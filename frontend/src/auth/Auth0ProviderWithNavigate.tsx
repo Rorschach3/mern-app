fbe02b66f0e4e2d0af13e697c1cc617f77cdd9e4
rorschach3 <daniel_fhernandez@yahoo.com>
2024-05-30 10:35:57 -0700

    update

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -12,7 +12,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+  const audience = "mern_food-ordering-app-api";
 
   if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Unable to initialize auth");

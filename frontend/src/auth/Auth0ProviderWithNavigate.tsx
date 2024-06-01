42ecf9cbafff0ee2fa15fd54fda59fb6b01a6f88
rorschach3 <daniel_fhernandez@yahoo.com>
2024-06-01 11:49:37 -0700

    Update user profile api

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -12,7 +12,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-  const audience = "mern_food-ordering-app-api";
+  const audience = import.meta.env.AUTH0_AUDIENCE;
 
   if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Unable to initialize auth");
